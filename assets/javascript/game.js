ea20dbe12f0fb278f4b33d7844b179055618d7b9
Jenny <jennyelisa07@aol.com>
2019-05-03 16:15:06 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,49 @@
-$(function() {
+// $(function() {
 
-    causeRepaintsOn = $("h1, h2, h3, p");
+//     causeRepaintsOn = $("h1, h2, h3, p");
 
-    $(window).resize(function() {
-        causeRepaintsOn.css("z-index", 1);
-    });
+//     $(window).resize(function() {
+//         causeRepaintsOn.css("z-index", 1);
+//     });
 
-});
+// });
+
+
+
+
+
+
+
+
+// var wins = 0;
+// var losses = 0;
+
+// var demo = document.getElementById('demo');
+// var directionsText = document.getElementById('directions-text');
+// var userGuess = document.getElementById('user-guess');
+// var winsText = document.getElementById('wins-text');
+// var currentWords = document.getElementById('current-words');
+// var guessesLeft = document.getElementById('guesses-left');
+// var lettersGuessed = document.getElementById('letters-guessed');
+// var blankSpace = ['_', '_', '_'];
+
+
+// <script>
+// var wordPool = ["Lemon", "Orange", "Apple", "Mango"];
+// document.getElementById("demo").innerHTML = wordPool;
+
+// var chosenWord = wordPool [Math.floor(Math.random() * wordPool.length)];
+// document.getElementById("demo").innerHTML = chosenWord;
+
+// var chosenLetters = chosenWord.split('');
+// document.getElementById("demo").innerHTML = chosenLetters;
+
+// function myFunction() {
+//   chosenLetter.push("_");
+//   document.getElementById("demo").innerHTML = chosenLetter;
+// }
+
+
+// </script>
+// document.onkeyup = function(event) {
+// var userGuess = event.key;
